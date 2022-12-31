e1cc508d589579c4d03bf653b37ef224ae2ac57b
Adam Gaia <git@agaia.dev>
2022-12-31 14:08:43 -0700

    feat: update commandstream lib

--- crates/ttydo/main.rs
+++ crates/ttydo/main.rs
@@ -3,7 +3,7 @@ use anyhow::{bail, Result};
 use clap::Parser;
 use log::debug;
 
-use commandstream::CommandStream;
+use commandstream::{CommandStream, SimpleCommand};
 
 /// Run a command with a fake tty
 #[derive(Parser, Debug)]
@@ -14,31 +14,6 @@ struct Args {
     command: Vec<String>,
 }
 
-struct TTYCommand<'a> {
-    command: &'a [String],
-}
-impl<'a> TTYCommand<'a> {
-    fn new(command: &'a [String]) -> Result<Self> {
-        Ok(TTYCommand { command })
-    }
-}
-
-impl<'a> CommandStream<'_> for TTYCommand<'a> {
-    fn command(&self) -> &[String] {
-        &self.command
-    }
-
-    fn handle_stdout(&self, line: &str) -> Result<()> {
-        println!("{}", line);
-        Ok(())
-    }
-
-    fn handle_stderr(&self, line: &str) -> Result<()> {
-        eprintln!("{}", line);
-        Ok(())
-    }
-}
-
 #[tokio::main]
 async fn main() -> Result<()> {
     env_logger::init();
@@ -47,7 +22,7 @@ async fn main() -> Result<()> {
     if command.is_empty() {
         bail!("Please specify a command to run following '--'");
     }
-    let app = TTYCommand::new(&command)?;
+    let app = SimpleCommand::new(&command)?;
     debug!("Running");
     let return_code = app.run().await?;
     debug!("Done");
